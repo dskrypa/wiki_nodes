8a04bb05c7e6c776922d9461f3d9c73c5022008b
dskrypa <dskrypa@gmail.com>
2020-02-16 12:34:07 -0500

    moved wiki nodes from ds_tools to its own repo

---
+++ readme.rst
@@ -0,0 +1,21 @@
+Wiki Nodes
+==========
+
+Library that provides
+
+Installation
+------------
+
+If installing on Linux, you should run the following first::
+
+    $ sudo apt-get install python3-dev
+
+
+Regardless of OS, setuptools is required::
+
+    $ pip3 install setuptools
+
+
+All of the other requirements are handled in setup.py, which will be run when you install like this::
+
+    $ pip3 install git+git://github.com/dskrypa/wiki_nodes
---
+++ setup.py
@@ -0,0 +1,39 @@
+
+from pathlib import Path
+from setuptools import setup
+
+project_root = Path(__file__).resolve().parent
+
+with project_root.joinpath('readme.rst').open('r', encoding='utf-8') as f:
+    long_description = f.read()
+
+about = {}
+with project_root.joinpath('wiki_nodes', '__version__.py').open('r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
+    long_description=long_description,
+    url=about['__url__'],
+    project_urls={'Source': about['__url__']},
+    packages=['wiki_nodes'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    python_requires='~=3.6',
+    install_requires=[
+        'requests_client @ git+git://github.com/dskrypa/requests_client',
+        'db_cache @ git+git://github.com/dskrypa/db_cache',
+        'wikitextparser',
+        'requests'
+    ]
+)
