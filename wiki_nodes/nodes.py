fbdd8bb313e4132f1eb2f8d1ba312fb7a101dc2a
dskrypa <dskrypa@gmail.com>
2020-03-29 07:33:13 -0400

    made BasicNodes hashable

--- wiki_nodes/nodes.py
+++ wiki_nodes/nodes.py
@@ -67,6 +67,9 @@ class BasicNode(Node):
     def __repr__(self):
         return f'<{self.__class__.__name__}({self.raw!r})>'
 
+    def __hash__(self):
+        return hash(self.raw.string)
+
     @property
     def is_basic(self):
         return True
