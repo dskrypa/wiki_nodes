b10bfad01dc17e0421a17694fb0e608eaede8eb0
dskrypa <dskrypa@gmail.com>
2021-05-02 11:22:23 -0400

    fixed setup requirement format

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ],
     python_requires='~=3.9',
     install_requires=[
-        'requests_client @ git+git://github.com/dskrypa/requests_client >=2021.5.2',
+        'requests_client>=2021.5.2 @ git+git://github.com/dskrypa/requests_client',
         'db_cache @ git+git://github.com/dskrypa/db_cache',
         'wikitextparser',
         'requests'
