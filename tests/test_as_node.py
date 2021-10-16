09479dc49cf9304c76087f5ebfaf47b4642f6bfe
dskrypa <dskrypa@gmail.com>
2021-10-16 17:08:35 -0400

    removed exploded version of test case

--- tests/test_as_node.py
+++ tests/test_as_node.py
@@ -14,22 +14,7 @@ log = logging.getLogger(__name__)
 
 class NodeParsingTest(TestCase):
     def test_no_lists(self):
-        # node = as_node("""'''Kim Tae-hyeong''' ({{Korean\n    | hangul  = 김태형\n    | hanja   =\n    | rr      =\n    | mr      =\n    | context =\n}}; born February 11, 1988), better known as '''Paul Kim''' ({{Korean\n    | hangul  = 폴킴\n    | hanja   =\n    | rr      =\n    | mr      =\n    | context =\n}}) is a South Korean singer. He debuted in 2014 and has released two extended plays and one full-length album in two parts: ''The Road'' (2017) and ''Tunnel'' (2018).""")
-        node = as_node("""'''Kim Tae-hyeong''' ({{Korean
-    | hangul  = 김태형
-    | hanja   =
-    | rr      =
-    | mr      =
-    | context =
-}}; born February 11, 1988), better known as '''Paul Kim''' ({{Korean
-    | hangul  = 폴킴
-    | hanja   =
-    | rr      =
-    | mr      =
-    | context =
-}}) is a South Korean singer. He debuted in 2014 and has released two extended plays and one full-length album in two parts: ''The Road'' (2017) and ''Tunnel'' (2018).
-"""
-        )
+        node = as_node("""'''Kim Tae-hyeong''' ({{Korean\n    | hangul  = 김태형\n    | hanja   =\n    | rr      =\n    | mr      =\n    | context =\n}}; born February 11, 1988), better known as '''Paul Kim''' ({{Korean\n    | hangul  = 폴킴\n    | hanja   =\n    | rr      =\n    | mr      =\n    | context =\n}}) is a South Korean singer. He debuted in 2014 and has released two extended plays and one full-length album in two parts: ''The Road'' (2017) and ''Tunnel'' (2018).""")
         self.assertTrue(not any(isinstance(n, List) for n in node))
 
     def test_link(self):
