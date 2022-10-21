8a71dce0296c6695bdd2804c2604c0dd588dfefc
dskrypa <dskrypa@gmail.com>
2022-10-21 16:49:57 -0400

    split out subcommands for wiki_page cli

--- bin/wiki_page.py
+++ bin/wiki_page.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
-from cli_command_parser import Command, Option, Flag, Positional, TriFlag, main
+from functools import cached_property
+from pathlib import Path
+
+from cli_command_parser import Command, Option, Flag, Positional, TriFlag, SubCommand, main, inputs
 
 from wiki_nodes.__version__ import __author_email__, __version__  # noqa
 
-MODES = ('raw', 'raw-pretty', 'headers', 'reprs', 'content', 'processed', 'title')
-
 
 class WikiPageViewer(Command, description='View a Wiki page', option_name_mode='-'):
-    url = Positional(help='A Wiki page URL')
-    mode = Option('-m', choices=MODES, default='raw', help='Page display mode')
-    recursive = TriFlag('-r', alt_short='-R', alt_prefix='not', help='Whether nodes should be printed recursively')
-    section = Option('-s', help='The section to view')
-    index = Option('-i', type=int, nargs=(1, 2), help='Index or slice within the selected node/section to view')
+    url: str
+    sub_cmd = SubCommand()
     debug = Flag('-d', help='Show debug logging')
-    categories = Flag('-c', help='Show page categories')
 
     def _init_command_(self):
         import logging
@@ -24,23 +21,33 @@ class WikiPageViewer(Command, description='View a Wiki page', option_name_mode='
         else:
             logging.basicConfig(level=logging.INFO, format='%(message)s')
 
+    @cached_property
+    def page(self):
+        from wiki_nodes import MediaWikiClient
+
+        return MediaWikiClient.page_for_article(self.url)
+
+
+class View(WikiPageViewer, help='View a wiki page'):
+    MODES = ('raw', 'raw-pretty', 'headers', 'reprs', 'content', 'processed')
+    url = Positional(help='A Wiki page URL')
+    mode = Option('-m', choices=MODES, default='raw', help='Page display mode')
+    recursive = TriFlag('-r', alt_short='-R', alt_prefix='not', help='Whether nodes should be printed recursively')
+    section = Option('-s', help='The section to view')
+    index = Option('-i', type=int, nargs=(1, 2), help='Index or slice within the selected node/section to view')
+
     def main(self):
-        if self.mode == 'title':
-            self.print_title()
-        else:
-            self.print_page()
+        kwargs = {'recurse': self.recursive} if self.recursive is not None else {}
+        for node in self.get_nodes():
+            node.pprint(self.mode, **kwargs)
 
-    def print_page(self):
-        from wiki_nodes import MediaWikiClient, Section
-
-        page = MediaWikiClient.page_for_article(self.url)
-        if self.categories:
-            self._print_categories(page)
+    def get_nodes(self):
+        from wiki_nodes import Section
 
         if self.section:
-            nodes = page.find_all(Section, title=self.section)
+            nodes = self.page.find_all(Section, title=self.section)
         else:
-            nodes = (page.sections,)
+            nodes = (self.page.sections,)
 
         if self.index:
             if len(self.index) > 1:
@@ -50,9 +57,15 @@ class WikiPageViewer(Command, description='View a Wiki page', option_name_mode='
                 index = self.index[0]
                 nodes = [node.processed()[index] for node in nodes]
 
-        kwargs = {'recurse': self.recursive} if self.recursive is not None else {}
-        for node in nodes:
-            node.pprint(self.mode, **kwargs)
+        return nodes
+
+
+class Meta(WikiPageViewer, help='View metadata about a given wiki page'):
+    url = Positional(help='A Wiki page URL')
+    categories = Flag('-c', help='Show page categories')
+
+    def main(self):
+        self.print_title()
 
     def print_title(self):
         from wiki_nodes.http import MediaWikiClient
@@ -60,10 +73,29 @@ class WikiPageViewer(Command, description='View a Wiki page', option_name_mode='
         client = MediaWikiClient(self.url, nopath=True)
         print(client.article_url_to_title(self.url))
         if self.categories:
-            self._print_categories(client.page_for_article(self.url))
+            self._print_categories()
 
-    def _print_categories(self, page):
-        print(f'Categories for {page.title}:\n' + '\n'.join(sorted(page.categories)))
+    def _print_categories(self):
+        print(f'Categories for {self.page.title}:\n' + '\n'.join(sorted(self.page.categories)))
+
+
+class Save(WikiPageViewer, help='Save a given wiki page'):
+    url = Positional(help='A Wiki page URL')
+    output: Path = Option('-o', type=inputs.Path(type='file|dir'), required=True, help='Output path')
+
+    def main(self):
+        text = self.page.raw.string
+        if self.output.is_dir() or not self.output.suffix:
+            path = self.output.joinpath(f'{self.page.title}.wiki')
+            if path.exists():
+                print(f'Warning: {path.as_posix()} already exists!')
+        else:
+            path = self.output
+
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with path.open('w', encoding='utf=8') as f:
+            print(f'Saving {self.page.title} to {self.output.as_posix()}')
+            f.write(text)
 
 
 if __name__ == '__main__':
